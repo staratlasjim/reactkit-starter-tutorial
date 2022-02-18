652b730a83a705b62ed2d529e753d3986fe8e72c
Jim Carter <jcarter@frigiddog.com>
2022-02-17 22:08:54 -0600

    - fixed bug where view did not get refactored to use the correct name of the new nft list view

--- src/pages/DisplayNft.tsx
+++ src/pages/DisplayNft.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next';
 import { AppBodyContainer } from '../ui/GlobalStyle.style';
-import { DisplayNftView } from '../ui/views/DisplayNftView/DisplayNftView';
+import { DisplayNftInfoView } from '../ui/views/DisplayNftInfoView/DisplayNftInfoView';
 
 const DisplayNft: NextPage = () => {
   return (
     <AppBodyContainer>
-      <DisplayNftView />
+      <DisplayNftInfoView />
     </AppBodyContainer>
   );
 };
